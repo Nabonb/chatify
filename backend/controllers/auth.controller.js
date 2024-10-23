fe57021f4e573e6b3ea8294ff50d68b11e55bfe8
Nabonb <nabobaruanb@gmail.com>
2024-10-23 13:37:29 +0600

    basic setup with mongoose and signup implemented

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,89 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
+
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+
+    // Here decrypt the password using bcrypt
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid username or password" });
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      gender: user.gender,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in login user: ", error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const logout = async (req, res) => {
+  console.log("Logout User");
+};
+
+export const signup = async (req, res) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+
+    if (password !== confirmPassword) {
+      res.status(400).json({ message: "Passwords do not match" });
+    }
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+      res.status(400).json({ message: "Username already exists" });
+    }
+
+    // Hash the password
+    const salt = await bcrypt.genSalt(10); // 10 is the number of rounds for hashing, you can adjust it as per your requirement.
+    const hashedPassword = await bcrypt.hash(password, salt); //hashing the password
+
+    // Create random avatar for the user
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = new User({
+      fullName,
+      username,
+      password: hashedPassword,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+
+    if (newUser) {
+      //Generate JWT token for the user
+      generateTokenAndSetCookie(newUser._id, res);
+      // Save the user to the database
+      newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        gender: newUser.gender,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid User Data" });
+    }
+  } catch (error) {
+    console.log("Error in signup user: ", error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
