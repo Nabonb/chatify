55a430f59322f92ea6e44f1cf85e728f860d9b2a
Nabonb <nabobaruanb@gmail.com>
2024-10-27 00:48:08 +0600

    sidebar logout button done

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -7,8 +7,6 @@ const Conversations = () => {
       <Conversation></Conversation>
       <Conversation></Conversation>
       <Conversation></Conversation>
-      <Conversation></Conversation>
-      <Conversation></Conversation>
     </div>
   );
 };
