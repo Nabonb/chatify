86ae3a64db2b7bf990d603689c5827e7cf1afe49
Nabonb <nabobaruanb@gmail.com>
2024-10-27 15:21:10 +0600

    message container ui part done

---
+++ frontend/src/components/messages/Message.jsx
@@ -0,0 +1,19 @@
+import demoUser from "../../assets/user.png";
+
+const Message = () => {
+  return (
+    <div className="chat chat-end">
+      <div className="chat-image avatar">
+        <div className="w-10 rounded-full">
+          <img alt="User Avatar" src={demoUser} />
+        </div>
+      </div>
+      <div className="chat-bubble text-white bg-blue-500">Hi There</div>
+      <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
+        12:41
+      </div>
+    </div>
+  );
+};
+
+export default Message;
---
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -0,0 +1,27 @@
+import demoUser from "../../assets/user.png";
+import MessageInput from "./MessageInput";
+import Messages from "./Messages";
+const MessageContainer = () => {
+  return (
+    <div className="md:min-w-[450px] flex flex-col">
+      <>
+        {/* This is the header */}
+        <div className="bg-slate-500 px-4 py-2 mb-2">
+          {/* Edited */}
+          <div className="flex gap-2">
+            <img className="h-6 w-6" src={demoUser} alt="" />
+            <span className="text-gray-900 font-bold"> John Doe</span>
+          </div>
+        </div>
+
+        {/* Messages */}
+        <Messages></Messages>
+
+        {/* Message Input */}
+        <MessageInput></MessageInput>
+      </>
+    </div>
+  );
+};
+
+export default MessageContainer;
