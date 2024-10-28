85cf7b913bc6a03ce0b147b0287b0c2de8b75b57
Nabonb <nabobaruanb@gmail.com>
2024-10-28 23:35:39 +0600

    signup functionality almost done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home/Home";
-// import Login from "./pages/login/Login.jsx";
-// import SignUp from "./pages/signup/Signup.jsx";
+import Login from "./pages/login/Login.jsx";
+import SignUp from "./pages/signup/Signup.jsx";
 
 function App() {
   return (
     <div className="p-4 flex justify-center items-center h-screen">
-      {/* <Login></Login> */}
-      {/* <SignUp></SignUp> */}
-      <Home></Home>
+      <Routes>
+        <Route path="/" element={<Home></Home>} />
+        <Route path="/login" element={<Login></Login>} />
+        <Route path="/signup" element={<SignUp></SignUp>} />
+      </Routes>
     </div>
   );
 }
--- frontend/src/pages/signup/GenderCheckbox.jsx
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -1,23 +1,33 @@
-const GenderCheckbox = () => {
+const GenderCheckbox = ({ handleCheckboxChange, selectGender }) => {
   return (
     <div className="flex p-2">
       <div className="form-control">
-        <label className="label gap-2 cursor-pointer">
+        <label
+          className={`label gap-2 cursor-pointer ${
+            selectGender === "male" ? "selected" : ""
+          }`}
+        >
           <span className="label-text">Male</span>
           <input
             type="checkbox"
-            defaultChecked
             className="checkbox border-slate-700"
+            onChange={() => handleCheckboxChange("male")}
+            checked={selectGender === "male"}
           />
         </label>
       </div>
       <div className="form-control ms-2">
-        <label className="label gap-2 cursor-pointer">
+        <label
+          className={`label gap-2 cursor-pointer ${
+            selectGender === "female" ? "selected" : ""
+          }`}
+        >
           <span className="label-text">Female</span>
           <input
             type="checkbox"
-            defaultChecked
             className="checkbox border-slate-700"
+            onChange={() => handleCheckboxChange("female")}
+            checked={selectGender === "female"}
           />
         </label>
       </div>
