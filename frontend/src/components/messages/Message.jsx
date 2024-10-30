fb3917537cc767246d23b2c3017500fc9f17e787
Nabonb <nabobaruanb@gmail.com>
2024-10-30 22:52:06 +0600

    get message and scroll down to bottom message and messages section functionality implemented

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,15 +1,30 @@
-import demoUser from "../../assets/user.png";
-const Message = () => {
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import useConverSation from "../../zustand/useConversation";
+import { extractTime } from "../../utils/extractTime";
+const Message = ({ message }) => {
+  const { authUser } = useContext(AuthContext);
+  const { selectedConversation } = useConverSation();
+  const fromMe = authUser._id === message.senderId;
+  const formattedTime = extractTime(message.createdAt);
+  const chatClassName = fromMe ? "chat-end" : "chat-start";
+  const profilePic = fromMe
+    ? authUser.profilePic
+    : selectedConversation?.profilePic;
+  const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+
   return (
-    <div className="chat chat-end">
+    <div className={`chat ${chatClassName}`}>
       <div className="chat-image avatar">
         <div className="w-10 rounded-full">
-          <img alt="User Avatar" src={demoUser} />
+          <img alt="User Avatar" src={profilePic} />
         </div>
       </div>
-      <div className="chat-bubble text-white bg-blue-500">Hi There</div>
+      <div className={`chat-bubble text-white ${bubbleBgColor}`}>
+        {message.message}
+      </div>
       <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
-        12:41
+        {formattedTime}
       </div>
     </div>
   );
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,19 +1,36 @@
+import { useEffect, useRef } from "react";
+import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
 import Message from "./Message";
 
 const Messages = () => {
+  const { loading, messages } = useGetMessages();
+  const lastMessageRef = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({ behavior: "smooth" }); // Scroll to the bottom of the messages container.
+    }, 100);
+  }, [messages]);
+
   return (
     <div className="px-4 flex-1 overflow-auto scrollbar-thin scrollbar-track-transparent">
-      <Message></Message>
-      <Message></Message>
-      <Message></Message>
-      <Message></Message>
-      <Message></Message>
-      <Message></Message>
-      <Message></Message>
-      <Message></Message>
-      <Message></Message>
-      <Message></Message>
-      <Message></Message>
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((message) => (
+          <div key={message._id} ref={lastMessageRef}>
+            <Message message={message} />
+          </div>
+        ))}
+
+      {loading &&
+        [...Array(6)].map((_, index) => <MessageSkeleton key={index} />)}
+
+      {!loading && messages.length === 0 && (
+        <p className="flex justify-center items-center h-full">
+          Send a message to start the conversation.
+        </p>
+      )}
     </div>
   );
 };
