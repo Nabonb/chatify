8ad719e7f0053a4c21345512341386f578c426c7
Nabonb <nabobaruanb@gmail.com>
2024-10-31 23:33:43 +0600

    Updated

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,6 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!!");
-});
-
 // app.use(cors({})); // enable CORS for all routes
 app.use(express.json()); // to parse JSON requests
 app.use(cookieParser()); // to parse cookies
