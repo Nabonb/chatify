dd57dc7b8c2f3c9ad2a153fb0fcf56da12a0196c
Nabonb <nabobaruanb@gmail.com>
2024-10-31 15:18:19 +0600

    socket.io basic setup and server with client connect succesfully

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,8 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
-// import cors from "cors";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
@@ -24,7 +23,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server is running on port ${PORT}..`);
 });
---
+++ backend/socket/socket.js
@@ -0,0 +1,38 @@
+import { Server } from "socket.io";
+import http from "http"; // buil in node.js http module
+import express from "express";
+
+const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+  },
+});
+
+const userSocketMap = {}; //{userId: socketId}
+
+// io.on() is a server-side event listener. It listens for incoming connections.
+io.on("connection", (socket) => {
+  console.log("User connected", socket.id);
+
+  const userId = socket.handshake.query.userId;
+
+  if (userId !== "undefined") {
+    userSocketMap[userId] = socket.id;
+  }
+
+  // io.emit() is a client-side event listener. It emits an event to all connected clients.I can use any name for the event. ex: getOnlineUsers
+  io.emit("getOnlineUsers", Object.keys(userSocketMap)); // Whoever connects or login it immediately sends the list of online users.
+
+  // socket.on() used to listen to the event. Can be used both server-side and client-side.
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
+    delete userSocketMap[userId]; // Remove user from the map when they disconnect.
+    io.emit("getOnlineUsers", Object.keys(userSocketMap)); // Send the updated list of online users to all connected clients.
+  });
+});
+
+export { app, server, io };
